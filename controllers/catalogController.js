b6676cd3c256868f6d749b02a3c23d335bdc924b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-25 11:28:49 +0000

    Create details page

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -1,19 +1,31 @@
 const router = require('express').Router();
-const { getAll } = require('../services/accomodationService');
+const { getAll, getById } = require('../services/accomodationService');
 
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
     const rooms = getAll();
-    
+
     res.render('catalog', {
         title: 'All Accomodation',
         rooms
     });
 });
 
-router.get('/:id', (req, res) =>{
-    res.render('details', {
-        title: 'Accomodation Details'
-    });
+router.get('/:id', (req, res) => {
+    const roomId = req.params.id;
+    const room = getById(roomId);
+
+    if (room) {
+        res.render('details', {
+            title: 'Accomodation Details',
+            room
+        });
+    } else{
+        res.render('roomNotFound', {
+            title: 'Accomodation Details',
+            roomId
+        });
+
+    }
 });
 
 
