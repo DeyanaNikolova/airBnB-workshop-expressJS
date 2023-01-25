0c465788a87550b04f21072cec03d47825a00840
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-25 09:37:43 +0000

    Create middleware and create page

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const hbs = require('express-handlebars').create({
 });
 
 const homeController = require('./controllers/homeController');
-const defaultController = require('./controllers/dafaultController');
 const catalogController = require('./controllers/catalogController');
+const createController = require('./controllers/createController');
+const defaultTitle = require('./middlewares/defaultTitle');
+const defaultController = require('./controllers/dafaultController');
+
+
+
 const app = express();
 
 app.engine('.hbs', hbs.engine);
@@ -14,9 +19,11 @@ app.set('view engine', '.hbs');
 app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('static'));
 
+app.use(defaultTitle('SoftUni Accomodation'));
+
 app.use(homeController);
 app.use('/catalog', catalogController);
-
+app.use('/create', createController);
 
 
 app.all('*', defaultController);
