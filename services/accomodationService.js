a601c7c3a7b325c1948d9441221c7f4ebfb22b25
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-25 12:55:47 +0000

    Add create functionality

--- services/accomodationService.js
+++ services/accomodationService.js
@@ -23,7 +23,28 @@ function getById(id) {
     return data.find(i => i.id == id);
 }
 
+async function create(roomData) {
+    const room = {
+        id: getId(),
+        name: roomData.name,
+        city: roomData.city,
+        beds: Number(roomData.beds),
+        price: Number(roomData.price),
+        imageUrl: roomData.imageUrl,
+        description: roomData.description
+
+    };
+    data.push(room);
+    await persist();
+    return room;
+}
+
+function getId() {
+    return ('000000' + (Math.random() * 999999 | 0).toString(16)).slice(-6);
+}
+
 module.exports = {
     getAll,
-    getById
+    getById,
+    create
 }
